388371ddd397e84c787b198d34c16c12f5ef336c
pinkieli <pinkie_li@163.com>
2018-04-02 08:59:04 +0800

    Delete some files

--- Delete/ModuleData.f90
+++
@@ -1,9 +0,0 @@
-MODULE ModuleData
-
-IMPLICIT NONE
-
-REAL,ALLOCATABLE:: K_Matrix(:,:), M_Matrix(:,:), C_Matrix(:,:), &
-				d0Vector(:), v0Vector(:), a0Vector(:)
-REAL :: NodalForceId
-
-END MODULE ModuleData
--- Delete/ModuleIoPort.f90
+++
@@ -1,7 +0,0 @@
-module ModuleIoPort
-
-integer :: InputFile=1000, InputData=1001
-integer :: RunDIMSD=2000
-INTEGER, DIMENSION(:),ALLOCATABLE :: OutputDisp,OutputVelo,OutputAcce
-
-end module ModuleIoPort
